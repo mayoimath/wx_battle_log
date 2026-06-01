6414b7601ca51d842d89d8013ab6fe17b0854604
高橋 健太 <mayoimath@gmail.com>
2026-06-01 20:05:39 +0900

    ホーム画面レイアウト修正(Gridに変更・レスポンシブ対応)

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,44 +1,72 @@
 import useFetchSummary from "@/hooks/UseFetchSummary";
 import { supabase } from "@/supabase/supabaseClient";
-import { Box, Button, Flex, Separator, Stack } from "@chakra-ui/react";
+import { Box, Button, Flex, Grid, GridItem, Separator, Stack, Text } from "@chakra-ui/react";
 import { Link, useNavigate } from "react-router";
 import { toaster } from "../ui/toaster";
+import React from "react";
 
 const Home = () => {
   const summary = useFetchSummary();
   const navigate = useNavigate();
   return (
-    <>
-      <Stack m={4} gap={4}>
-        {summary.map((x, index) => (
-          <Stack key={index}>
-            <Flex gap={4} align="center">
+    <Stack m={4}>
+      {/* Header */}
+      <Grid templateColumns={{ base: "1fr 1fr 1fr", md: "3fr 1fr 1fr 1fr" }} mx={4}>
+        <GridItem colSpan={{ base: 3, md: 1 }}>
+          <Text fontWeight="bold">Title</Text>
+        </GridItem>
+        <GridItem>
+          <Text fontWeight="bold">Lrig</Text>
+        </GridItem>
+        <GridItem>
+          <Text fontWeight="bold">Result</Text>
+        </GridItem>
+        <GridItem></GridItem>
+      </Grid>
+      <Separator />
+      {/* Details */}
+      {summary.map((x, index) => (
+        <React.Fragment key={index}>
+          <Grid templateColumns={{ base: "1fr 1fr 1fr", md: "3fr 1fr 1fr 1fr" }} alignItems="center" mx={4}>
+            <GridItem colSpan={{ base: 3, md: 1 }}>
               <Box>{x.title}</Box>
-              <Box>使用ルリグ : {x.lrig}</Box>
+            </GridItem>
+            <GridItem>
+              <Box>{x.lrig}</Box>
+            </GridItem>
+            <GridItem>
               <Box>
                 {x.wonCount}-{x.loseCount}
               </Box>
-              <Button mx={4} asChild>
-                <Link to={`/battle_log/${x.logNo}`}>編集</Link>
-              </Button>
-            </Flex>
-            <Separator />
-          </Stack>
-        )) ?? <></>}
-      </Stack>
-      <Button m={4} asChild>
-        <Link to="/battle_log">新規登録</Link>
-      </Button>
-      <Button
-        onClick={async () => {
-          await supabase.auth.signOut();
-          toaster.create({ title: "サインアウト", type: "info" });
-          navigate("/login");
-        }}
-      >
-        サインアウト
-      </Button>
-    </>
+            </GridItem>
+            <GridItem>
+              <Flex gap={4}>
+                <Button asChild>
+                  <Link to={`/battle_log/${x.logNo}`}>編集</Link>
+                </Button>
+                <Button>削除</Button>
+              </Flex>
+            </GridItem>
+          </Grid>
+          <Separator />
+        </React.Fragment>
+      )) ?? <></>}
+      <Flex>
+        <Button m={4} asChild>
+          <Link to="/battle_log">新規登録</Link>
+        </Button>
+        <Button
+          m={4}
+          onClick={async () => {
+            await supabase.auth.signOut();
+            toaster.create({ title: "サインアウト", type: "info" });
+            navigate("/login");
+          }}
+        >
+          サインアウト
+        </Button>
+      </Flex>
+    </Stack>
   );
 };
 
