701e9de56ab890e478c726400280133043128e8d
高橋 健太 <mayoimath@gmail.com>
2026-06-15 20:11:56 +0900

    ラジオカードコンパクト化

--- src/components/atoms/PrimaryRadioCard.tsx
+++ src/components/atoms/PrimaryRadioCard.tsx
@@ -6,14 +6,14 @@ import { RadioCardItem } from "../ui/radio-card";
 type Props = {
   value: string;
   onValueChange?: ((details: ValueChangeDetails) => void) | undefined;
-  title: string;
+  title?: string | null;
   options: Array<OptionItem>;
 };
 
 const PrimaryRadioCard = ({ value, onValueChange, title, options }: Props) => {
   return (
-    <RadioCard.Root value={value} onValueChange={onValueChange}>
-      <RadioCard.Label>{title}</RadioCard.Label>
+    <RadioCard.Root value={value} onValueChange={onValueChange} size="sm">
+      {title ?? <RadioCard.Label>{title}</RadioCard.Label>}
       <HStack>
         {options.map((option) => (
           <RadioCardItem {...option} />
