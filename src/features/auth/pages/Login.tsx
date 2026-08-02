52843765877ce607a7c04ce6ef639d9ff5fb1525
高橋 健太 <mayoimath@gmail.com>
2026-08-02 19:41:55 +0900

    ログイン画面 エラーメッセージ取得を関数化

--- src/features/auth/pages/Login.tsx
+++ src/features/auth/pages/Login.tsx
@@ -4,13 +4,38 @@ import { useNavigate } from "react-router";
 import { toaster } from "@/components/ui/toaster";
 import { PasswordInput } from "@/components/ui/password-input";
 import useAuth from "../hooks/UseAuth";
+import type { AuthError } from "@supabase/supabase-js";
 
 type FormValue = {
   email: string;
   password: string;
 };
 
-function Login() {
+const getSignInErrorMessage = (error: AuthError) => {
+  switch (error.code) {
+    case "invalid_credentials":
+      return "ユーザ情報が登録されていません。";
+    default:
+      return "不明なエラー";
+  }
+};
+
+const getSignUpErrorMessage = (error: AuthError) => {
+  switch (error.code) {
+    case "invalid_credentials":
+      return "ユーザ情報が登録できません。";
+    case "user_already_exists":
+      return "そのユーザはすでに登録されています。";
+    case "validation_failed":
+      return "入力された情報の形式に誤りがあります。";
+    case "weak_password":
+      return "パスワードが短すぎます。";
+    default:
+      return "不明なエラー";
+  }
+};
+
+const Login = () => {
   const { register, handleSubmit } = useForm<FormValue>({ defaultValues: { email: "", password: "" } });
   const navigate = useNavigate();
   const { signIn, signUp } = useAuth();
@@ -20,53 +45,40 @@ function Login() {
         if (mode == "signin") {
           const error = await signIn(field.email, field.password);
           if (error) {
-            console.log(error);
-            if (error.code == "invalid_credentials")
-              toaster.create({ title: "サインイン失敗", description: "ユーザ情報が登録されていません。", type: "error" });
-            else toaster.create({ title: "サインイン失敗", description: "不明なエラー", type: "error" });
-          } else {
-            toaster.create({ title: "サインイン", type: "success" });
-            navigate("/");
+            toaster.create({ title: "ログイン失敗", description: getSignInErrorMessage(error), type: "error" });
+            return;
           }
+          toaster.create({ title: "ログイン", type: "success" });
+          navigate("/");
         } else {
           const error = await signUp(field.email, field.password);
           if (error) {
-            console.log(error);
-            if (error.code == "invalid_credentials")
-              toaster.create({ title: "サインアップ失敗", description: "ユーザ情報が登録できません。", type: "error" });
-            else if (error.code == "user_already_exists")
-              toaster.create({ title: "サインアップ失敗", description: "そのユーザはすでに登録されています。", type: "error" });
-            else if (error.code == "validation_failed") toaster.create({ title: "サインアップ失敗", description: "形式不正です。", type: "error" });
-            else if (error.code == "weak_password")
-              toaster.create({ title: "サインアップ失敗", description: "パスワードが短すぎます。", type: "error" });
-            else toaster.create({ title: "サインアップ失敗", description: "不明なエラー", type: "error" });
-          } else {
-            toaster.create({ title: "サインアップ", type: "success" });
-            navigate("/");
+            toaster.create({ title: "ユーザー登録失敗", description: getSignUpErrorMessage(error), type: "error" });
+            return;
           }
+          toaster.create({ title: "ユーザー登録成功", type: "success" });
+          navigate("/");
         }
       })(),
     );
   return (
-    <form>
-      <Flex justify="center" m={12}>
-        <Stack width={{ base: "100%", md: "50%" }}>
-          <Field.Root>
-            <Field.Label>メールアドレス</Field.Label>
-            <Input {...register("email")} placeholder="xxxx@yy.zz" />
-          </Field.Root>
-          <Field.Root>
-            <Field.Label>パスワード</Field.Label>
-            <PasswordInput {...register("password")} />
-          </Field.Root>
-          <Flex justify="space-around" m={4}>
-            <Button onClick={onSubmit("signin")}>サインイン</Button>
-            <Button onClick={onSubmit("signup")}>サインアップ</Button>
-          </Flex>
-        </Stack>
-      </Flex>
-    </form>
+    <Flex justify="center" px={12} align="center" as="form" h="full">
+      <Stack width={{ base: "100%", md: "50%" }}>
+        <Field.Root>
+          <Field.Label>メールアドレス</Field.Label>
+          <Input {...register("email")} placeholder="xxxx@yy.zz" />
+        </Field.Root>
+        <Field.Root>
+          <Field.Label>パスワード</Field.Label>
+          <PasswordInput {...register("password")} />
+        </Field.Root>
+        <Flex justify="space-around" m={4}>
+          <Button onClick={onSubmit("signin")}>ログイン</Button>
+          <Button onClick={onSubmit("signup")}>新規登録</Button>
+        </Flex>
+      </Stack>
+    </Flex>
   );
-}
+};
 
 export default Login;
